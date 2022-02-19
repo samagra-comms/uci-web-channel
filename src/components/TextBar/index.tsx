4c71a946a684f916ea24da11f448a463bac963b0
Shruti Agarwal <mail2shruti.ag@gmail.com>
2022-02-19 20:57:43 +0530

    refactor: functional components
    Signed-off-by: Shruti Agarwal <mail2shruti.ag@gmail.com>

--- src/components/TextBar/index.tsx
+++ src/components/TextBar/index.tsx
@@ -1,46 +1,37 @@
+import { useRef } from "react";
 import { Box, Button, Input } from "@chakra-ui/react";
-import React, { Component } from "react";
-
 import { MdSend } from "react-icons/md";
 
-export default class TextBar extends Component {
-  props: any;
-  input: any;
-  constructor(props: any) {
-    super(props);
-    this.props = props;
-    this.input = React.createRef();
-  }
-  sendMessage() {
-    this.props.onSend && this.props.onSend(this.input.current.value);
-    this.input.current.value = "";
-  }
-  sendMessageIfEnter(e: any) {
+const TextBar = (props: any) => {
+  const input: any = useRef(null);
+  const sendMessage = () => {
+    props?.onSend && props.onSend(input.current.value);
+    input.current.value = "";
+  };
+  const sendMessageIfEnter = (e: any) => {
     if (e.keyCode === 13) {
-      this.sendMessage();
+      sendMessage();
     }
-  }
-  render() {
-    const sendMessage = this.sendMessage.bind(this);
-    const sendMessageIfEnter = this.sendMessageIfEnter.bind(this);
+  };
 
-    return (
-      <Box bg="transparent" w="100%" p={4} color="white">
-        <Input
-          w="80%"
-          placeholder="Type your message"
-          ref={this.input}
-          onKeyDown={sendMessageIfEnter}
-        />
-        <Button
-          leftIcon={<MdSend />}
-          colorScheme="pink"
-          variant="solid"
-          onClick={sendMessage}
-        >
-          Send
-        </Button>
-      </Box>
-    );
-  }
-}
+  return (
+    <Box bg="transparent" w="100%" p={4} color="white">
+      <Input
+        w="80%"
+        placeholder="Type your message"
+        ref={input}
+        onKeyDown={sendMessageIfEnter}
+      />
+      <Button
+        leftIcon={<MdSend />}
+        colorScheme="pink"
+        variant="solid"
+        onClick={sendMessage}
+      >
+        Send
+      </Button>
+    </Box>
+  );
+};
+
+export default TextBar;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as serviceWorker from "serviceWorker";
 
-import { App } from "App";
+import App from "App";
 import { ColorModeScript } from "@chakra-ui/react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "reportWebVitals";
