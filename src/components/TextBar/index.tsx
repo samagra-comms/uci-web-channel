ca52b632c63123cfd44e5ac3fa06ae0b2926c08c
Shruti Agarwal <mail2shruti.ag@gmail.com>
2022-02-22 07:37:21 +0530

    fix: ui
    Signed-off-by: Shruti Agarwal <mail2shruti.ag@gmail.com>

--- src/components/TextBar/index.tsx
+++ src/components/TextBar/index.tsx
@@ -4,33 +4,73 @@ import { MdSend } from "react-icons/md";
 
 const TextBar = (props: any) => {
   const input: any = useRef(null);
-  const sendMessage = () => {
+  const sendMessage = (e: any) => {
+    e.preventDefault();
     props?.onSend && props.onSend(input.current.value);
     input.current.value = "";
   };
   const sendMessageIfEnter = (e: any) => {
     if (e.keyCode === 13) {
-      sendMessage();
+      sendMessage(e);
     }
   };
 
   return (
-    <div className="chat-footer">
-      <Input
-        w="80%"
-        placeholder="Type your message"
-        ref={input}
-        onKeyDown={sendMessageIfEnter}
-      />
-      <Button
-        leftIcon={<MdSend />}
-        colorScheme="pink"
-        variant="solid"
-        onClick={sendMessage}
-      >
-        Send
-      </Button>
-    </div>
+    <>
+      {/* <div className="chat__footer" onBlur={handleBlur} >
+	        <form>
+	            <input
+	            	ref={inputRef}
+	                value={input}
+	                onClick={handleFocus}
+	                onChange={!recording ? change : null}
+	                onKeyPress={recording ? () => false : null}
+	                onFocus={() => setFocus(true)}
+	                placeholder="Type a message"
+	            />
+	           
+		        	<>
+		        		<label
+			        		for="capture"  
+			            	class="send__btn" 
+			            >
+			                {btnIcons}
+			            </label> 
+			        	<input
+			        		style={{display: "none"}} 
+				        	type="file" 
+				            id="capture"  {navigator.mediaDevices.getUserMedia && window.MediaRecorder ?
+                      <button 
+                        type="submit" 
+                        class="send__btn" 
+                        onClick={input !== "" || (input === "" && image) ? sendMessage : startRecording}
+                      >
+                          {btnIcons}
+                      </button>	
+                  
+				            accept="audio/*" 
+				            capture
+				            onChange={audioInputChange}  
+			            />
+		        	</>
+		        
+	            
+	        </form>
+	        
+	    </div> */}
+      <div className="chat__footer">
+        <form>
+          <input
+            placeholder="Type your message"
+            ref={input}
+            onKeyDown={sendMessageIfEnter}
+          />        
+        <button className="send__btn" onClick={sendMessage} type="submit">
+          Send
+        </button>
+        </form>
+      </div>
+    </>
   );
 };
 
