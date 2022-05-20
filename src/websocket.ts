4d0f3ff1ae1bae311a40f6e698ab5b9ebdbf1577
tanishachaudhary <chumunch0102@gmail.com>
2022-05-20 12:31:40 +0530

    media files

--- src/websocket.ts
+++ src/websocket.ts
@@ -4,7 +4,7 @@ const host =
     : "localhost:3005";
 const { io } = require("socket.io-client");
 
-export const socket = io("ws://ts-tanisha1.ngrok.samagra.io");
+export const socket = io("ws://ts-tanisha.ngrok.samagra.io");
 export const send = (msg: any, session: any) =>
   socket.emit("botRequest", {
     content: {
