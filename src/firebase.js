3d128a5baaf3d77d7ec42523ab7f64cdc9455bde
Arshpreet Singh <arshpreets2807@gmail.com>
2022-05-30 03:37:37 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -23,8 +23,7 @@ import { getMessaging, getToken, onMessage } from "firebase/messaging";
 var firebaseConfig = {
     apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: "testing-uci-web-channel",
-    // projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
+    projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.PUBLIC_FIREBASE_APP_ID,
