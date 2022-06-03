70d177fe350e6dc6375a0c8572a5312fa5c13ebf
ChakshuGautam <chaks.gautam@gmail.com>
2022-06-03 11:04:54 +0530

    Fixes: #17

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -5,4 +5,12 @@ context("E2E", () => {
     // add E2E test commands here
     cy.wrap(true).should("equal", true);
   });
+
+  it("render response from starting message", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("input").type("Hi UCI", { force: true });
+    cy.get("button").click({ force: true });
+    cy.wait(1000);
+    cy.get("div").contains("Hi! RozgarBot welcomes you!");
+  });
 });
