acc7c2e16826586dc5e1641fbec0bc72c53517e1
Neelesh2512 <neeleshsharma2512@gmail.com>
2022-06-27 13:20:51 +0530

    Nextjs Implementation

---
+++ .next/static/hAcNJKL3NzZPgb1z-TN5c/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/54858d1ea55d0074.css","static/chunks/pages/index-3d5d6b36b5243ae0.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ src/components/websocket.ts
@@ -0,0 +1,48 @@
+import {io} from 'socket.io-client';
+// const host =
+//   process.env.NODE_ENV === "production"
+//     ? window.location.host
+//     : "localhost:3005";
+
+
+export const socket = io(`${process.env.NEXT_PUBLIC_TRANSPORT_SOCKET_URL}`);
+export const send = (msg: any, session: any) =>
+  socket.emit("botRequest", {
+    content: {
+      text: msg,
+      userId: session.userID,
+      appId: "appId",
+      channel: "diksha",
+      from: session.socketID,
+      context: null,
+    },
+    to: "admin",
+  });
+
+export const startWebsocketConnection = () => {
+  socket.on("connect", () => {
+    console.log(`opened ws connection ${socket.id}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("close ws connection");
+  });
+
+  socket.on("botResponse", (arg: any) => {
+    onMessageCallback && onMessageCallback(arg);
+  });
+
+  socket.on("session", (arg: any) => {
+    onSessionCallback && onSessionCallback(arg);
+  });
+};
+
+let onMessageCallback: (arg0: any) => any,
+  onSessionCallback: (arg0: any) => any;
+export const registerOnMessageCallback = (fn: (msg: any) => void) => {
+  onMessageCallback = fn;
+};
+
+export const registerOnSessionCallback = (fn: (session: any) => void) => {
+  onSessionCallback = fn;
+};
