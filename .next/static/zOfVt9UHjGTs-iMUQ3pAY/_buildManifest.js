6e879d65ee63e217ccfedd522732757c57b0ca37
Neelesh2512 <neeleshsharma2512@gmail.com>
2022-06-27 14:45:21 +0530

    resolving build issues

--- .next/static/zOfVt9UHjGTs-iMUQ3pAY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/54858d1ea55d0074.css","static/chunks/pages/index-32e61befabe1f30f.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
