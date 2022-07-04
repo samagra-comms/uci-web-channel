77755e9e91aa495314e967a10eb0d67079e5fb5f
Arshpreet Singh <arshpreets2807@gmail.com>
2022-07-04 18:26:08 +0530

    API for authorization created: /api/auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,14 +13,13 @@ const Home: NextPage = () => {
   return (
     <React.StrictMode>
       <Head>
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <meta
           name="description"
           content="Web site created using create-react-app"
         />
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
         <title>UCI PWA</title>
       </Head>
       <CookiesProvider>
