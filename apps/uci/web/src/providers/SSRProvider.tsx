2fc350410c58cc0f83bd5b1742c4d415aece0f6a
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2023-05-31 22:36:40 +0530

    hydration-fixed

--- apps/uci/web/src/providers/SSRProvider.tsx
+++ apps/uci/web/src/providers/SSRProvider.tsx
@@ -2,8 +2,8 @@ import React, { FC } from 'react'
 import { ContextProvider } from './ContextProvider';
 
 const SSRProvider: FC<{ children:  React.ReactNode }> = ({ children }) => {
-   if (typeof window === 'undefined') 
-   return null
+//    if (typeof window === 'undefined') 
+//    return null
     return <ContextProvider>{children}</ContextProvider>
 }
 
