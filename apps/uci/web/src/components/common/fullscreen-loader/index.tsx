0f7493f5a255fc97b2200a2e97e7baa29bf5dafc
Tushar98644 <b.toshar@iitg.ac.in>
2023-08-24 13:58:10 +0530

    Fullscreen Loader

--- apps/uci/web/src/components/common/fullscreen-loader/index.tsx
+++ apps/uci/web/src/components/common/fullscreen-loader/index.tsx
@@ -1,15 +1,34 @@
-import React, { FC } from 'react';
-import styles from './index.module.css';
+import React, { FC, useEffect, useState } from 'react';
+import './index.css';
 import { Modal, ModalContent, ModalOverlay } from '@chakra-ui/react';
 
-export const FullScreenLoader: FC<{ loading: boolean }> = ({ loading }) => (
-    <Modal isCentered isOpen={loading} onClose={() => null}>
-        <ModalOverlay
-            bg="blackAlpha.300"
-            backdropFilter="blur(10px) hue-rotate(90deg)"
-        />
-        <ModalContent className={`${styles.modalContent}`}>
-            <div id="loader" className={`${styles.spinner}`}></div>
-        </ModalContent>
-    </Modal>
-);
+export const FullScreenLoader: FC<{ loading: boolean }> = ({ loading }) => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <Modal isCentered isOpen={isLoading || loading} onClose={() => null}>
+            <ModalOverlay />
+            <ModalContent>
+                <section className="wrapper">
+                    <div className="spinner">
+                        <i></i>
+                        <i></i>
+                        <i></i>
+                        <i></i>
+                        <i></i>
+                        <i></i>
+                        <i></i>
+                    </div>
+                </section>
+            </ModalContent>
+        </Modal>
+    );
+};
